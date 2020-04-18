116a0a9af924efd164e148bc68b11f8b21a3eb62
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-04-18 23:42:31 +0900

    feat(home):PetComponentの作成と紐付け

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pet } from 'src/app/interfaces/pet';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  pet: Pet = {
+    avatarURL: `/assets/images/pet-1.png`,
+    name: `テストペット`,
+    level: 4,
+    exp: 200
+  };
 
   constructor() { }
 
