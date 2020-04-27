fe78c4d9c2dac36675c6472a4ac7fb75781bf4a2
Takumi Uchida <bmpni23786@yahoo.co.jp>
2020-04-27 21:11:01 +0900

    kk

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { PetComponent } from './pet/pet.component';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ExpPipe } from '../exp.pipe';
 
 
